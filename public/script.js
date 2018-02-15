968a8579bba73e693d8c6e5f4ada52fb8337f654
pho7on <nikdu4@gmail.com>
2018-02-15 16:36:38 +0100

    Testing123

--- public/script.js
+++ public/script.js
@@ -3,10 +3,9 @@ $.ajax({
     type: "GET",
     contentType: "application/json"
 }).done(function (images) {}).then(function (images) {
-
     images.forEach((image) => {
         var absPath = image.location;
-        var pathArr = absPath.split('\\');
+        var pathArr = absPath.split('/');
         var relPath = pathArr[2];
         $('body').append(`<img src='/images/${relPath}'></img>`)
         console.log(relPath);
